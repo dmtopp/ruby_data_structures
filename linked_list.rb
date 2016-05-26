3bd30124aad7b11c3f9d6cf458ff3e0b6edf8164
Daniel Topp <dmtopp@gmail.com>
2016-05-26 17:32:28 -0500

    Implemented node and linked list classes

---
+++ linked_list.rb
@@ -0,0 +1,81 @@
+class LinkedList
+
+  def initialize
+    @head = nil
+  end
+
+  def to_s
+    node = @head
+    puts node.value
+
+    while node.link
+      node = node.link
+      puts node.value
+    end
+
+    puts '---------------'
+
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+
+    if @head
+      node = @head
+      while node.link
+        node = node.link
+      end
+      node.link = new_node
+    else
+      @head = new_node
+    end
+
+  end
+
+  # append a new node to a 0-indexed position
+  def append_to_pos(value, position)
+    new_node = Node.new(value)
+
+    if !@head
+      @head = new_node
+    elsif position == 0
+      new_node.link = @head
+      @head = new_node
+    else
+      node = @head
+      # go to the position before where we want to append
+      (0...position - 1).each do
+        node = node.link
+      end
+
+      new_node.link = node.link
+      node.link = new_node
+
+    end
+
+  end
+
+  # get the value of a node at a position
+  def get_node(position)
+    node = @head
+
+    (0...position).each do
+      node = node.link
+    end
+
+    node.value
+  end
+
+  def set_node(value, position)
+    node = @head
+
+    (0...position).each do
+      node = node.link
+    end
+
+    node.value = value
+
+  end
+
+
+end # end class
---
+++ test.rb
@@ -0,0 +1,30 @@
+require './node.rb'
+require './linked_list.rb'
+
+my_sexy_node = Node.new('hi')
+
+puts my_sexy_node.value
+puts my_sexy_node.link
+
+my_sexy_linked_list = LinkedList.new()
+my_sexy_linked_list.append('hi')
+my_sexy_linked_list.append('yo')
+my_sexy_linked_list.append('swag')
+# my_sexy_linked_list.to_s
+my_sexy_linked_list.append_to_pos('XD', 1)
+# my_sexy_linked_list.to_s
+my_sexy_linked_list.append_to_pos('buh', 0)
+# my_sexy_linked_list.to_s
+my_sexy_linked_list.append_to_pos('fuh', 2)
+# my_sexy_linked_list.to_s
+my_sexy_linked_list.append_to_pos('luh', 4)
+my_sexy_linked_list.to_s
+
+puts my_sexy_linked_list.get_node(4)
+puts my_sexy_linked_list.get_node(0)
+puts '---------------'
+
+my_sexy_linked_list.set_node('x', 0)
+my_sexy_linked_list.set_node('xXx', 3)
+
+my_sexy_linked_list.to_s
