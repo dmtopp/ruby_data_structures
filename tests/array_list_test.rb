ebfe9c958f7aba03bc13f1e295fe16f6326d15a7
Daniel Topp <dmtopp@gmail.com>
2016-05-27 13:39:39 -0500

    finished tests for ArrayList

--- tests/array_list_test.rb
+++ tests/array_list_test.rb
@@ -15,12 +15,31 @@ describe ArrayList do
 
   it 'should get the value of an element' do
     @array_list.append(test_val_one)
+
     expect(@array_list.get_pos(0)).to eq(test_val_one)
+  end
+
+  it 'should set the value of an element' do
+    @array_list.append(test_val_one)
+    @array_list.set_pos(test_val_two, 0)
+
+    expect(@array_list.get_pos(0)).to eq(test_val_two)
   end
 
   it 'should append new elements to the end of the array' do
     @array_list.append(test_val_one)
     @array_list.append(test_val_two)
+
     expect(@array_list.get_pos(1)).to eq(test_val_two)
   end
+
+  it 'should raise an exception if we attempt to get an index that is out of bounds' do
+    expect{@array_list.get_pos(100)}.to raise_exception('Index out of bounds!')
+  end
+
+  it 'should raise an exception if we attempt to set an index that is out of bounds' do
+    expect{@array_list.set_pos(test_val_one, 100)}.to raise_exception('Index out of bounds!')
+  end
+
+
 end
